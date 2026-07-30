91364b884b59108cac2800b4b09b786629647395
annali-portfolio <annali-portfolio@github.com>
2026-07-30 01:11:01 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-30T01:11:01.304Z

--- src/main/main.py
+++ src/main/main.py
@@ -100,9 +100,10 @@ def load_and_clean_call_logs(file_path):
                     break
             clean_call_logs.append(split_line)
 
-    with open("callLogs.csv", "w") as user_list:
-        csv_writer = csv.writer("callLogs.csv")
-        csv_writer.writerows(clean_call_logs)
+    with open("callLogs.csv", "w") as call_logs:
+        csv_writer = csv.writer(call_logs)
+        for item in clean_call_logs:
+            cvs_writer.writerow(item)
 
     print("TODO: load_call_logs")
 
