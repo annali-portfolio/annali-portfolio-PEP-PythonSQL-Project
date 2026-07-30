af63816c72f559152306177e1ee4279e1dcd5034
annali-portfolio <annali-portfolio@github.com>
2026-07-30 22:41:04 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-30T22:41:04.559Z

--- src/main/main.py
+++ src/main/main.py
@@ -143,11 +143,6 @@ def write_user_analytics(csv_file_path):
 
 
 
-
-
-
-
-
 # This function will write the callLogs ordered by userId, then start time.
 # Then, write the ordered callLogs to orderedCalls.csv
 def write_ordered_calls(csv_file_path):
