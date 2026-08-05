5c91d341ca83fb5cfaad9db2ecf40050bd17115d
annali-portfolio <annali-portfolio@github.com>
2026-08-05 04:11:55 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-08-05T04:11:55.798Z

--- src/main/main.py
+++ src/main/main.py
@@ -146,10 +146,10 @@ def write_user_analytics(csv_file_path):
 # Then, write the ordered callLogs to orderedCalls.csv
 def write_ordered_calls(csv_file_path):
 
-    # cursor.execute("""
-    # SELECT * FROM callLogs
-    # ORDER BY userID, startTime
-    # """)
+    cursor.execute("""
+    SELECT * FROM callLogs
+    ORDER BY userID, startTime
+    """)
 
     # ordered_call_logs_select = cursor.fetchall()
 
@@ -172,7 +172,7 @@ def write_ordered_calls(csv_file_path):
     print("TODO: write_ordered_calls")
     
 
-callId,phoneNumber,startTime,endTime,direction,userId
+
 
 
 
