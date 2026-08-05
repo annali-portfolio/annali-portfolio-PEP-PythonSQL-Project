0f4bbaccb3538f52bceb4050ce2edaa85d8dfbeb
annali-portfolio <annali-portfolio@github.com>
2026-08-05 04:38:44 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-08-05T04:38:44.834Z

--- src/main/main.py
+++ src/main/main.py
@@ -152,7 +152,7 @@ def write_ordered_calls(csv_file_path):
 
     ordered_call_logs_select = cursor.fetchall()
 
-    with open(csv_file_path, "w") as ordered_call_logs_csv:
+    with open(csv_file_path, "a+") as ordered_call_logs_csv:
         ordered_call_logs_csv.write("\n")
         writer = csv.writer(ordered_call_logs_csv)
         writer.writerows(ordered_call_logs_select)
